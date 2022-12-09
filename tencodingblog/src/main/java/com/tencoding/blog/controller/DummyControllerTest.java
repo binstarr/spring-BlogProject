956453ed3398e6708220a43229eeb12f91472452
binstarr <tjdqls2s@naver.com>
2022-12-09 11:21:29 +0900

    update 및 더티 체킹

--- tencodingblog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/DummyControllerTest.java
@@ -2,6 +2,8 @@ package com.tencoding.blog.controller;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -72,5 +75,29 @@ public class DummyControllerTest {
 		
 		return "회원가입이 완료 되었습니다.";
 	}
+	
+	// 회원 수정
+	@Transactional // 영속화할 때 걸어준다. & 트랜잭션 기본 의미 : 함수 종료시에 더티 체킹을 하고 수정된 데이터가 있다면 commit이 된다.
+	@PutMapping("/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User reqUser) {
+		
+		log.info(">>>> id : {}, >>>>pw : {}, >>>>email : {}", id, reqUser.getPassword(), reqUser.getEmail());
+		
+		// 사용자 여부를 먼저 확인 (select)
+		// 사용자 있다면 넘겨 받은 데이터를 가공해서  -> 저장
+		// 사용자가 없다면 클라이언트에게 잘못된 요청, 없는 사용자 입니다.
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패 하였습니다.");
+		});
+		user.setPassword(reqUser.getPassword());
+		user.setEmail(reqUser.getEmail());
+//		userRepository.save(user);
+		
+		return null;
+	}
+	
+	
+	
 
 }
