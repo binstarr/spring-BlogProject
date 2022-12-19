ad86c11751ad426fd74572e2652cdc1ec92713cb
binstarr <tjdqls2s@naver.com>
2022-12-19 12:13:37 +0900

    페이징처리2

--- tencodingblog/src/main/java/com/tencoding/blog/controller/BoardController.java
+++ tencodingblog/src/main/java/com/tencoding/blog/controller/BoardController.java
@@ -1,5 +1,7 @@
 package com.tencoding.blog.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,13 +38,42 @@ public class BoardController {
 			@PageableDefault(size = 3, sort = "id", direction = Direction.DESC) Pageable pageable) {
 
 		Page<Board> boards = boardService.getBoardList(pageable);
-		model.addAttribute("boards", boards);
+		int PAGENATION_BLOCK_COUNT = 3;
 		// jsp 파일에서 model 추상객체를 이용해서 컨트롤러에서 내려 준 데이터에 접근이 가능하다.
+		
+		// 1. 현재 페이지 앞 뒤로 2칸씩 보이기
+		// 2. 현재 페이지 active 처리 하기
+		// 3. 페이지 숫자를 눌렀을 경우 해당 페이지 화면 이동하기
+		// 마지막에 보여야 하는 페이지를 잘 확인하자!
+		
+		// 총 게시물에서 화면에 보여줄 게시물을 계산을 하면 총 몇 페이지가 나오는지 알 수 있다. 
+		System.out.println(">>>>>화면에 보여줄 게시글의 갯수이다." + boards.getSize());
+		System.out.println(">>>>>전체 페이지 크기" + boards.getTotalPages());
+		System.out.println(">>>>>현재 페이지 번호" + boards.getPageable().getPageNumber());
+		
+		int nowPage = boards.getPageable().getPageNumber() + 1;
+		// 밑에 번호가 앞 뒤로 2칸씩 보이게 만든다
+		int startPageNumber = Math.max(nowPage - PAGENATION_BLOCK_COUNT, 1);  //nowPage - 2;
+		int endPageNumber = Math.min(nowPage + PAGENATION_BLOCK_COUNT, boards.getTotalPages()); //nowPage + 2;
+		System.out.println("시작해야 하는 번호 : " + startPageNumber);
+		System.out.println("마지막에 보여야 하는 번호 : " + endPageNumber);
+		// 블럭 숫자에는 - 값을 보여 주면 안된다.
+		
+		// 자료구조를 만들어서 반복문을 돌린다.
+		ArrayList<Integer> pageNumbers = new ArrayList<>();
+		for(int i = startPageNumber; i<= endPageNumber; i++) {
+			pageNumbers.add(i);
+		}
+		
+		model.addAttribute("boards", boards);
+		model.addAttribute("nowPage", nowPage);
+		model.addAttribute("startPage", startPageNumber);
+		model.addAttribute("endPage", endPageNumber);
+		model.addAttribute("pageNumbers", pageNumbers);
+
 
 		return "index";
 	}
-	// page. first = true, false <--첫번째 페이지면 true
-	// page. last = true, false <-- 마지막 페이지면 ture
 
 	@GetMapping("/board/save_form")
 	public String saveForm() {
--- tencodingblog/src/main/webapp/WEB-INF/views/index.jsp
+++ tencodingblog/src/main/webapp/WEB-INF/views/index.jsp
@@ -14,10 +14,23 @@
 <ul class="pagination justify-content-center">
 	<c:set var="isDisabled" value="disabled"></c:set>
 	<c:set var="isNotDisabled" value=""></c:set>
-  <li class="page-item ${boards.first ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number-1}">Previous</a></li>
-  <!-- 총 페이지가 몇개인지 알아야 한다. -->
-  <!-- 10개가 있으면 페이지당 3개로 지정했으니 페이지가 4개가 나와야 한다. -->
-  <li class="page-item ${boards.last ? isDisabled: isNotDisabled}"><a class="page-link" href="?page=${boards.number+1}">Next</a></li>
+	<li class="page-item ${boards.first ? isDisabled : isNotDisabled}"><a class="page-link" href="?page=${boards.number-1}">Previous</a></li>
+	<!-- 총 페이지가 몇개인지 알아야 한다. -->
+	<!-- 10개가 있으면 페이지당 3개로 지정했으니 페이지가 4개가 나와야 한다. -->
+	<!-- 반복문 처리하기 123, 1234, 12345 -->
+	<c:forEach var="num" items="${pageNumbers}">
+		<c:choose>
+			<c:when test="${nowPage eq num}">
+				<li class="page-item active"><a class="page-link" href="?page=${num - 1}">${num}</a></li>
+			</c:when>
+			<c:otherwise>
+				<li class="page-item"><a class="page-link" href="?page=${num - 1}">${num}</a></li>
+			</c:otherwise>
+		</c:choose>
+
+	</c:forEach>
+
+	<li class="page-item ${boards.last ? isDisabled: isNotDisabled}"><a class="page-link" href="?page=${boards.number+1}">Next</a></li>
 </ul>
 
 
